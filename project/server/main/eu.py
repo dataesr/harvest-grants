375d6eb49b246efee80429a725eb0c8591ef8026
eric <eric.jeangirard@recherche.gouv.fr>
2026-09-18 11:15:17 +0200

    fixes

--- project/server/main/eu.py
+++ project/server/main/eu.py
@@ -9,18 +9,42 @@ logger = get_logger(__name__)
 
 eu_url = "https://api.tech.ec.europa.eu/search-api/prod/rest/search"
 eu_headers = {"Accept": "application/json, text/plain, */*"}
-eu_query = {
-    "bool": {
-        "must": [
-            {"terms": {"DATASOURCE": ["SEDIA_NONH2020_PROD"]}},
-            {"terms": {"status": ["Ongoing", "Ended"]}},
-            {"terms": {"language": ["en"]}},
-        ],
-        "must_not": [
-            {"terms": {"programId": ["43108390", "31045243"]}},  # H2020, Horizon
-        ],
+
+MAPPING = {
+        'h2020': '31045243',
+        'he': '43108390',
+        'erasmus': '43353764',
+        'crea': '43251814',
+        'cerv': '43251589',
+        'smp': '43252476',
+        'cef': '43251567',
+        'life': '43252405',
+        'comp': '31059643',
+        'dig': '43152860'
+        }
+
+def get_eu_query(case):
+    assert(case in list(MAPPING.keys()) + ['rest'] )
+    eu_query = {
+        "bool": {
+            "must": [
+                {"terms": {"DATASOURCE": ["SEDIA_NONH2020_PROD"]}},
+                {"terms": {"status": ["Ongoing", "Ended"]}},
+                {"terms": {"language": ["en"]}},
+            ],
+            "must_not": [
+                {"terms": {"programId": [MAPPING['he'], MAPPING['h2020']]}},  # H2020, Horizon
+            ],
+        }
     }
-}
+    if case == 'base':
+        return eu_query
+    elif case in MAPPING:
+        eu_query['bool']['must'].append( {"terms": {"programId": [MAPPING[case]]}})
+    elif case == 'rest':
+        eu_query['bool']['must_not'] =  [{"terms": {"programId": list(MAPPING.values())}}]
+    return eu_query
+
 eu_sort = {"order": "DESC", "field": "es_SortDate"}
 eu_fields = [
     "title",
@@ -47,36 +71,39 @@ eu_fields = [
     "typeOfActions",
 ]
 eu_languages = ["en"]
-eu_files = {
-    "query": ("blob", json.dumps(eu_query), "application/json"),
-    "sort": ("blob", json.dumps(eu_sort), "application/json"),
-    "displayFields": ("blob", json.dumps(eu_fields), "application/json"),
-    "languages": ("blob", json.dumps(eu_languages), "application/json"),
-}
+def get_eu_files(case):
+    eu_files = {
+      "query": ("blob", json.dumps(get_eu_query(case)), "application/json"),
+        "sort": ("blob", json.dumps(eu_sort), "application/json"),
+        "displayFields": ("blob", json.dumps(eu_fields), "application/json"),
+        "languages": ("blob", json.dumps(eu_languages), "application/json"),
+    }
+    return eu_files
+
 eu_params = {"apiKey": "SEDIA_NONH2020_PROD", "text": "***", "pageSize": 1, "pageNumber": 1}
 
 
 @retry(delay=20, tries=3)
-def fetch_one_page(page_number: int, page_size: int) -> list:
+def fetch_one_page(case, page_number: int, page_size: int) -> list:
     params = {**eu_params, "pageSize": page_size, "pageNumber": page_number}
     response = requests.post(
         url=eu_url,
         headers=eu_headers,
-        files=eu_files,
+        files=get_eu_files(case),
         params=params,
     )
     data = response.json()
     return data
 
 
-def fetch_all(page_size: int = 50):
+def fetch_all(case, page_size: int = 50):
     next_page = 1
     results = []
-    logger.info("Start fetching EU API...")
+    logger.info(f"Start fetching EU API for {case} ...")
 
     while next_page > 0:
         logger.debug(f"Fetching page {next_page}...")
-        data = fetch_one_page(next_page, page_size)
+        data = fetch_one_page(case, next_page, page_size)
         results.extend(data.get("results", []))
 
         next_page = next_page + 1 if data["totalResults"] > (page_size * next_page) else 0
@@ -154,35 +181,49 @@ def extract_projects(data: list, cache_participant: dict) -> list:
         project_ids.add(project_id)
 
         project["id"] = project_id
-        project["url"] = metadata["url"][0]
+        if metadata.get("url") and len(metadata.get("url")):
+            project["url"] = metadata["url"][0]
         #project["type"] = metadata["programAbbreviation"][0]  # TODO mapping ?
         project["type"] = 'Autres financements européens'
 
-        project["startDate"] = metadata["startDate"][0]
-        project["endDate"] = metadata["endDate"][0]
-        project["signatureDate"] = metadata["ecSignatureDate"][0]
-        project["year"] = project["startDate"][0:4]
+        if metadata.get("startDate") and len(metadata.get("startDate")):
+            project["startDate"] = metadata["startDate"][0]
+            project["year"] = project["startDate"][0:4]
+        if len(metadata.get("endDate")):
+            project["endDate"] = metadata["endDate"][0]
+        if metadata.get("ecSignatureDate") and len(metadata.get("ecSignatureDate")):
+            project["signatureDate"] = metadata["ecSignatureDate"][0]
 
-        project["acronym"] = {"default": metadata["acronym"][0]}
-        project["label"] = {"default": metadata["title"][0]}
-        if len(metadata["objective"]):
+        if len(metadata.get("acronym")):
+            project["acronym"] = {"default": metadata["acronym"][0]}
+        if len(metadata.get("title")):
+            project["label"] = {"default": metadata["title"][0]}
+        if len(metadata.get("objective")):
             project["description"] = {"default": metadata["objective"][0]}
 
-        project["participantCount"] = metadata["numberOfContributors"][0]
+        if metadata.get("numberOfContributors") and len(metadata.get("numberOfContributors")):
+            project["participantCount"] = metadata["numberOfContributors"][0]
+
         project["participants"] = extract_participants(project_id, metadata["participants"][0], cache_participant)
 
-        if len(metadata["overallBudget"]):
+        if metadata.get("overallBudget") and len(metadata.get("overallBudget")):
             project["budgetTotal"] = metadata["overallBudget"][0]
-        if len(metadata["euContributionAmount"]):
+        if metadata.get("euContributionAmount") and len(metadata.get("euContributionAmount")):
             project["budgetFinanced"] = metadata["euContributionAmount"][0]
 
-        project["callIdentifier"] = {"id": metadata["callIdentifier"][0]}
+        if metadata.get("callIdentifier") and len(metadata.get("callIdentifier")):
+            project["callIdentifier"] = {"id": metadata["callIdentifier"][0]}
 
-        action_code = metadata["typeOfActions"][0]
-        action_label = metadata["typeOfAction"][0]
-        program = metadata['programmes'][0]
-        program_id = metadata['programId'][0]
-        program_acronym = metadata['programAbbreviation'][0]
+        if metadata.get("typeOfActions") and len(metadata.get("typeOfActions")):
+            action_code = metadata["typeOfActions"][0]
+        if metadata.get("typeOfAction") and len(metadata.get("typeOfAction")):
+            action_label = metadata["typeOfAction"][0]
+        if metadata.get("programmes") and len(metadata.get("programmes")):
+            program = metadata['programmes'][0]
+        if metadata.get("programId") and len(metadata.get("programId")):
+            program_id = metadata['programId'][0]
+        if metadata.get("programAbbreviation") and len(metadata.get("programAbbreviation")):
+            program_acronym = metadata['programAbbreviation'][0]
         project["instrument"] = action_label
         project["action"] = {
             "code": program_id,
@@ -213,7 +254,13 @@ def extract_projects(data: list, cache_participant: dict) -> list:
 
 
 def harvest_eu_projects(cache_participant: dict) -> list:
-    results = fetch_all()
+    # Il Faut découper car il y a plus de 10000 projets à récupérer
+    results = []
+    for c in MAPPING:
+        if c in ['he', 'h2020']:
+            continue
+        results += fetch_all(c)
+    results += fetch_all('rest')
     projects = extract_projects(results, cache_participant)
 
     if len(projects):
